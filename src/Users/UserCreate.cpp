c140ede3cf2ad3fb5a958f42b5de3d179b4493f3
mihail <mihail.latyshov2@mail.ru>
2023-06-16 02:29:15 +0300

    add some fixes (user create)

--- src/Users/UserCreate.cpp
+++ src/Users/UserCreate.cpp
@@ -1,6 +1,7 @@
 #include "UserCreate.hpp"
 
 #include <fmt/format.h>
+#include <string>
 
 #include <userver/clients/dns/component.hpp>
 #include <userver/formats/json.hpp>
@@ -17,10 +18,10 @@ class UserCreate final : public userver::server::handlers::HttpHandlerBase {
  public:
   static constexpr std::string_view kName = "handler-user-create";
 
-  Hello(const userver::components::ComponentConfig& config,
-        const userver::components::ComponentContext& component_context)
+  UserCreate(const userver::components::ComponentConfig& config,
+             const userver::components::ComponentContext& component_context)
       : HttpHandlerBase(config, component_context),
-        pg_cluster_(
+        m_PGCluster(
             component_context
                 .FindComponent<userver::components::Postgres>("postgres-db-1")
                 .GetCluster()) {}
@@ -28,30 +29,33 @@ class UserCreate final : public userver::server::handlers::HttpHandlerBase {
   std::string HandleRequestThrow(
       const userver::server::http::HttpRequest& request,
       userver::server::request::RequestContext&) const override {
-    const auto& nickname = request.GetArg("nickname");
-    formats::json::Value json =
+    const auto& nickname = request.GetPathArg("nickname");
+    userver::formats::json::Value json =
         userver::formats::json::FromString(request.RequestBody());
     const auto fullname = json["fullname"].As<std::string>("");
-    const auto about = json["about"].As<std::string>();
+    const auto about = json["about"].As<std::string>("");
     const auto email = json["email"].As<std::string>("");
 
-    if (!name.empty() && !fullname.empty(), &&!email.empty()) {
-      auto result = pg_cluster_->Execute(
+    if (!nickname.empty() && !fullname.empty() && !email.empty()) {
+      auto result = m_PGCluster->Execute(
           userver::storages::postgres::ClusterHostType::kMaster,
           "INSERT INTO tp.users(nickname, fullname, about, email)"
           "VALUES($1, $2, $3, $4)",
           nickname, fullname, about, email);
-      return "Created!"
+
+      return fmt::format("Created, {}!\n", result.RowsAffected());
     }
 
-    return "Error!"
+    return "Error!";
   }
 
   userver::storages::postgres::ClusterPtr m_PGCluster;
 };
 
+}  // namespace
+
 void AppendUserCreate(userver::components::ComponentList& component_list) {
   component_list.Append<UserCreate>();
 }
 
-}  // namespace
+}  // namespace vkpg
